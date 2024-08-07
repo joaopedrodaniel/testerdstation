34a2a6e8ff606fdfe4c405665ca0cfa90717a616
João Pedro Daniel <joaopedro.daniel@estudante.ufjf.br>
2024-08-06 22:29:23 -0300

    feito básico do básico

---
+++ app/formulario/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Formulario() {
+    return (
+      <div className="flex flex-col w-full bg-yellow-200 min-h-screen items-center gap-10">
+        <h1 className="pt-10">Testando RD Station - Formulário</h1>
+        <Link href={"/"}>
+          <button className="bg-red-400" >Voltar pra página principal</button>
+        </Link>
+      </div>
+    );
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="flex flex-col w-full bg-yellow-200 min-h-screen items-center gap-10">
+      <h1 className="pt-10">Testando RD Station - Página principal</h1>
+      <Link href={"/formulario"}>
+        <button className="bg-red-400" >Ir para página do formulário</button>
+      </Link>
     </div>
   );
 }
